a9eb94b32ad62cd371f2d832343adebf561d2417
Pushkar Singh <ssingh.ebay@gmail.com>
2015-10-28 18:33:34 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "My Webpage"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "My Webpage"
   end
 
 end
